84e249de7a9fc65153a424cd5b6255af9b30c2da
mrperrytpx <tomislav.siprak1996@gmail.com>
2023-08-12 14:06:32 +0200

    tectus

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tectus.rocks tribute
+
+There used to be a website like this made by [wonderfulheaven](https://twitter.com/wonderfulheaven), but it is no longer hosted.
---
+++ index.js
@@ -0,0 +1,51 @@
+const body = document.getElementById("body");
+let didUserInteract = false;
+let activeTectide = 0;
+let tectusImages = 0;
+
+function createTectus() {
+    tectusImages++;
+    if (activeTectide >= 5) return;
+
+    const riseMountinsAudio = new Audio("public/rise-mountains.mp3");
+
+    const tectus = document.createElement("img");
+    tectus.setAttribute("src", "public/tectus.webp");
+    tectus.setAttribute("id", "tectus");
+
+    riseMountinsAudio.addEventListener("ended", () => {
+        activeTectide--;
+        createTectus();
+        createTectus();
+        if (tectusImages >= 200) body.removeChild(tectus);
+    });
+
+    const delay = Math.random() * 3000;
+    setTimeout(() => {
+        riseMountinsAudio.play();
+
+        let topPosition = Math.floor(Math.random() * body.clientHeight - 150);
+        let leftPosition = Math.floor(Math.random() * body.clientWidth - 150);
+
+        console.log(topPosition, leftPosition);
+
+        tectus.style.top = topPosition + "px";
+        tectus.style.left = leftPosition + "px";
+        body.appendChild(tectus);
+        activeTectide++;
+    }, delay);
+}
+
+function userInteracted() {
+    const whatIsThisAudio = new Audio("public/what-is-this.mp3");
+    whatIsThisAudio.play();
+    whatIsThisAudio.addEventListener("ended", () => {
+        createTectus();
+    });
+}
+
+body.addEventListener("click", () => {
+    if (didUserInteract === true) return;
+    didUserInteract = true;
+    userInteracted();
+});
